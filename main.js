69d170b609f3728d7ec22b388c87dd2ff4287acc
D-Arora <iamdharamarora@outlook.com>
2022-03-21 23:20:44 +1100

    Scroll on chrome mobile

--- main.js
+++ main.js
@@ -1,45 +1,47 @@
-const slider = document.querySelector(".slides");
-const preventClick = (e) => {
-  e.preventDefault();
-  e.stopImmediatePropagation();
-}
-let isDown = false;
-let isDragged = false;
-let startX;
-let scrollLeft;
+if(window.innerWidth > 480) {
+	const slider = document.querySelector(".slides");
+	const preventClick = (e) => {
+	e.preventDefault();
+	e.stopImmediatePropagation();
+	}
+	let isDown = false;
+	let isDragged = false;
+	let startX;
+	let scrollLeft;
 
-slider.addEventListener("mousedown", e => {
-  isDown = true;
-  slider.classList.add("active");
-  startX = e.pageX - slider.offsetLeft;
-  scrollLeft = slider.scrollLeft;
-});
-slider.addEventListener("mouseleave", () => {
-  isDown = false;
-  slider.classList.remove("active");
-});
-slider.addEventListener("mouseup", (e) => {
-  isDown = false;
-  const elements = document.querySelectorAll(".slidetangle");
-  if(isDragged){
-	  for(let i = 0; i<elements.length; i++){
-			elements[i].addEventListener("click", preventClick);
-	  }
-  }
-  else{
-	  for(let i = 0; i<elements.length; i++){
-			elements[i].removeEventListener("click", preventClick);
-	  }
-  }
-  slider.classList.remove("active");
-  isDragged =  false;
-});
-slider.addEventListener("mousemove", e => {
-  if (!isDown) return;
-  isDragged =  true;
-  e.preventDefault();
-  const x = e.pageX - slider.offsetLeft;
-  const walk = (x - startX) * 3;
-  slider.scrollLeft = scrollLeft - walk;
-  console.log(walk);
-});
+	slider.addEventListener("mousedown", e => {
+	isDown = true;
+	slider.classList.add("active");
+	startX = e.pageX - slider.offsetLeft;
+	scrollLeft = slider.scrollLeft;
+	});
+	slider.addEventListener("mouseleave", () => {
+	isDown = false;
+	slider.classList.remove("active");
+	});
+	slider.addEventListener("mouseup", (e) => {
+	isDown = false;
+	const elements = document.querySelectorAll(".slidetangle");
+	if(isDragged){
+		for(let i = 0; i<elements.length; i++){
+				elements[i].addEventListener("click", preventClick);
+		}
+	}
+	else{
+		for(let i = 0; i<elements.length; i++){
+				elements[i].removeEventListener("click", preventClick);
+		}
+	}
+	slider.classList.remove("active");
+	isDragged =  false;
+	});
+	slider.addEventListener("mousemove", e => {
+	if (!isDown) return;
+	isDragged =  true;
+	e.preventDefault();
+	const x = e.pageX - slider.offsetLeft;
+	const walk = (x - startX) * 3;
+	slider.scrollLeft = scrollLeft - walk;
+	console.log(walk);
+	});
+}
